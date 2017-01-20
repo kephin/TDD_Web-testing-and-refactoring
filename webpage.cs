8d9e4e60b8c6ab1fe961eb90d2220903bfa34d39
kephin <wunshon76325@gmail.com>
2017-01-20 17:42:03 +0800

    seperate web UI from logic by viewModel

--- webpage.cs
+++ webpage.cs
@@ -15,66 +15,98 @@ public partial class Product : System.Web.UI.Page
   {
     if (this.IsValid)
     {
-      if (this.drpCompany.SelectedValue == "1")
+      var product = this.GetShippingProduct();
+      if (product.Shipper == 1)
       {
         this.lblCompany.Text = "黑貓";
-        var weight = Convert.ToDouble(this.txtProductWeight.Text);
+        // 選擇黑貓計算運費
+
+        var weight = product.Weight;
         if (weight > 20)
         {
-          this.lblCharge.Text = "500";
+          product.ShippingFee = 500;
         }
         else
         {
           var fee = 100 + weight * 10;
-          this.lblCharge.Text = fee.ToString();
+          product.ShippingFee = fee;
         }
       }
-      else if (this.drpCompany.SelectedValue == "2")
+      else if (product.Shipper == 2)
       {
         this.lblCompany.Text = "新竹貨運";
-        var length = Convert.ToDouble(this.txtProductLength.Text);
-        var width = Convert.ToDouble(this.txtProductWidth.Text);
-        var height = Convert.ToDouble(this.txtProductHeight.Text);
+        // 選擇新竹貨運計算運費
 
-        var size = length * width * height;
+        var size = product.Size.Length * product.Size.Width * product.Size.Height;
 
         //長 x 寬 x 高（公分）x 0.0000353
-        if (length > 100 || width > 100 || height > 100)
+        if (product.Size.Length > 100 || product.Size.Width > 100 || product.Size.Height > 100)
         {
-          this.lblCharge.Text = (size * 0.0000353 * 1100 + 500).ToString();
+          product.ShippingFee = size * 0.0000353 * 1100 + 500;
         }
         else
         {
-          this.lblCharge.Text = (size * 0.0000353 * 1200).ToString();
+          product.ShippingFee = size * 0.0000353 * 1200;
         }
       }
-      else if (this.drpCompany.SelectedValue == "3")
+      else if (product.Shipper == 3)
       {
         this.lblCompany.Text = "郵局";
+        // 選擇郵局計算運費
 
-        var weight = Convert.ToDouble(this.txtProductWeight.Text);
-        var feeByWeight = 80 + weight * 10;
+        var feeByWeight = 80 + product.Weight * 10;
 
-        var length = Convert.ToDouble(this.txtProductLength.Text);
-        var width = Convert.ToDouble(this.txtProductWidth.Text);
-        var height = Convert.ToDouble(this.txtProductHeight.Text);
-        var size = length * width * height;
+        var size = product.Size.Length * product.Size.Width * product.Size.Height;
+
         var feeBySize = size * 0.0000353 * 1100;
 
         if (feeByWeight < feeBySize)
         {
-          this.lblCharge.Text = feeByWeight.ToString();
+          product.ShippingFee = feeByWeight;
         }
         else
         {
-          this.lblCharge.Text = feeBySize.ToString();
+          product.ShippingFee = feeBySize;
         }
       }
       else
       {
-        var js = "alert('發生不預期錯誤，請洽系統管理者');location.href='http://tw.yahoo.com/';";
-        this.ClientScript.RegisterStartupScript(this.GetType(), "back", js, true);
+          var js = "alert('發生不預期錯誤，請洽系統管理者');location.href='http://tw.yahoo.com/';";
+          this.ClientScript.RegisterStartupScript(this.GetType(), "back", js, true);
       }
+      this.lblCharge.Text = product.ShippingFee.ToString();
     }
   }
+  private ShippingProduct GetShippingProduct()
+  {
+    var result = new ShippingProduct
+    {
+      Name = this.txtProductName.Text,
+      Weight = Convert.ToDouble(this.txtProductWeight.Text),
+      Size = new Size
+      {
+        Length = Convert.ToDouble(this.txtProductLength.Text),
+        Width = Convert.ToDouble(this.txtProductWeight),
+        Height = Convert.ToDouble(this.txtProductHeight),
+      },
+      Shipper = Convert.ToInt32(this.drpCompany.SelectedValue),
+    };
+    return result;
+  }
+}
+
+public class ShippingProduct
+{
+  public string Name { get; set; }
+  public double Weight { get; set; }
+  public Size Size { get; set; }
+  public int Shipper { get; set; }
+  public double ShippingFee { get; set; }
+}
+
+public struct Size
+{
+  public double Length { get; set; }
+  public double Width { get; set; }
+  public double Height { get; set; }
 }
